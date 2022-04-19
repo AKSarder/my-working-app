15ca40528dac9b38979d35ef9a736f6f61e48647
AKSarder <63703796+AKSarder@users.noreply.github.com>
2022-04-19 16:18:17 +0600

    done

---
+++ src/Components/Countries/Countries.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const countries = () => {
+    const [countries, setCountries] = useState([])
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default countries;
